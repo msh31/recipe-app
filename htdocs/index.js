7028462c188b4cc35d2884017e5c942d0f281eae
Ids Osinga <ip.osinga@alfa-college.nl>
2025-05-09 13:35:19 +0200

    v2025-05-09

--- htdocs/index.js
+++ htdocs/index.js
@@ -1,8 +1,18 @@
-//let amounts = document.querySelector(".amount")
-let amount_for = document.getElementById("amount_for").value
-console.log(amount_for)
+let amounts = document.querySelectorAll(".amount")
+let old_amounts = [];
+amounts.forEach(amount => {
+    old_amounts.push(amount.innerText);
+});
+let oud_aantal = document.getElementById("amount_for").value
+console.log("aantal personen = " + amount_for)
 
 function changeIngred()
 {
-    alert("YES");
+    let nieuw_aantal = document.getElementById("amount_for").value
+    let factor = nieuw_aantal / oud_aantal;
+    let i = 0;
+    amounts.forEach(amount => {
+        amount.innerText = old_amounts[i] * factor;
+        i++;
+     });
 }
